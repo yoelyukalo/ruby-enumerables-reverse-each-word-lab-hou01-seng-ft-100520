bba93855ffd51e75c450980718a8a64a51093e15
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-24 01:14:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(string)
-  
-  
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
