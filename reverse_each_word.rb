86183dae4a25ddd27d0069bd4c5c039d7e4fea08
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-24 01:39:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 require 'pry'
 def reverse_each_word(string)
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
+  def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
   end
-  return_array.join(" ")
-  binding.pry
+  test_array.join(" ")
 end
